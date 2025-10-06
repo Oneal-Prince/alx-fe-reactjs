64d650f5481fbdfcf4e1365947ef48c9c04357e2
monia <ornellagrace0412@gmail.com>
2025-10-06 19:28:29 +0200

    add

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import recipesData from "../data.json";
+import { Link, Links } from "react-router-dom";
 function HomePage() {
 
   const [recipes, setRecipes] = useState([]);
@@ -15,24 +16,26 @@ function HomePage() {
 
       <div className="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {recipes.map((recipe) => (
-          <div
+           <Link
             key={recipe.id}
+            to={`/recipes/${recipe.id}`}
             className="bg-white rounded-xl shadow-md overflow-hidden transform transition duration-300 hover:shadow-xl hover:scale-105"
           >
-            <img
-              src={recipe.image}
-              alt={recipe.title}
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-5">
-              <h2 className="text-2xl font-semibold text-gray-800 mb-2">{recipe.title}</h2>
-              <p className="text-gray-600">{recipe.summary}</p>
-            </div>
-          </div>
-        ))}
+      <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="w-full h-48 object-cover"
+      />
+      <div className="p-5">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-2">{recipe.title}</h2>
+          <p className="text-gray-600">{recipe.summary}</p>
       </div>
-    </div>
+  </Link>
+))}
 
+      </div>
+      </div>
+  
 
    );
 }
