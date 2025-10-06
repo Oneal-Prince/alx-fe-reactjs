e4b6b7f0149d6b8e6993d4f0fe684ce7c1b9f036
monia <ornellagrace0412@gmail.com>
2025-10-06 18:10:29 +0200

    add

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,15 @@
-
-import HomePage from './components/HomePage'
-import RecipeDetail from './components/RecipeDetail'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
-  
-
   return (
-    <>
-    <HomePage />
-    <RecipeDetail />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
